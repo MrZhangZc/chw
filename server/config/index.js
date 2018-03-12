37ec9aa55df13d4db65d5d6a7c8613060f67005a
zzc <1761997216@qq.com>
2018-03-12 11:03:29 +0800

    config3

--- server/config/index.js
+++ server/config/index.js
@@ -1,9 +1,9 @@
 export default {
-	// db: 'mongodb://localhost/chw',
-	// SITE_ROOT_URL: 'http://cb4wpm4.hk1.mofasuidao.cn',
-	// wechat : {
-	// 	appID: 'wx8b7e922a82db90c8',
-	// 	token: 'csxhwechat',
-	// 	appSecret: 'e0161299d68744e01b3142b2f86a9060'
-	// }
+	db: 'mongodb://localhost/chw',
+	SITE_ROOT_URL: 'http://cb4wpm4.hk1.mofasuidao.cn',
+	wechat : {
+		appID: 'wx8b7e922a82db90c8',
+		token: 'csxhwechat',
+		appSecret: 'e0161299d68744e01b3142b2f86a9060'
+	}
 }
